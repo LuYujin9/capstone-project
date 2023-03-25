fcd96b89ebb163ed2ee0acf31828f98f81cf9483
YujinLu9 <wuwujane@hotmail.com>
2023-03-25 15:17:03 +0100

    added Heading

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 
 const Heading = styled.section`
+  width: 100vw;
+  height: 4rem;
+  margin: 0;
+
   text-align: center;
+  position: fixed;
+  top: 0;
+
   background-color: var(--rosehip-color);
+  color: white;
 `;
 
 export default function Head() {
-  return <Heading>Restaurants</Heading>;
+  return (
+    <Heading>
+      <h1>Restaurants</h1>
+    </Heading>
+  );
 }
