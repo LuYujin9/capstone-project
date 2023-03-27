1f049795304d1bd9ab25d2ffd31835d349036a86
YujinLu9 <wuwujane@hotmail.com>
2023-03-27 17:43:24 +0200

    add notice in test

--- components/Heading/Heading.test.js
+++ components/Heading/Heading.test.js
@@ -1,6 +1,7 @@
 import Heading from "./Heading";
 import { render, screen } from "@testing-library/react";
 
+//test for back button has not finished.
 test("test Heading content", () => {
   render(<Heading>title</Heading>);
   const element = screen.getByText(/title/i);
