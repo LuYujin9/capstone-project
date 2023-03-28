4a5e14577986f8ec346d3cf32682324345a7722a
Yujin Lu <123362292+LuYujin9@users.noreply.github.com>
2023-03-28 10:42:45 +0200

    Update List.styles.js
    deleted the empty "StyledDescription"

--- components/List/List.styles.js
+++ components/List/List.styles.js
@@ -8,7 +8,9 @@ export const StyledImage = styled(Image)`
 export const StyledList = styled.ul`
   margin: 5rem 1vw;
   padding: 0;
+  
   display: grid;
+  
   @media only screen and (max-width: 799px) {
     gird: 80vw;
   }
@@ -23,11 +25,14 @@ export const StyledList = styled.ul`
 export const StyledLi = styled.li`
   margin: 0.5rem 0.5rem;
   padding: 5%;
-  border-radius: 1rem;
+  width: 95%;
+ 
   display: block;
+  
+  border-radius: 1rem;
   background-color: var(--white-color);
   box-shadow: 2px 2px 10px 3px var(--linen-color);
-  width: 95%;
+
 `;
 
 export const StyledName = styled.p`
@@ -35,20 +40,22 @@ export const StyledName = styled.p`
   font-size: 1.2rem;
 `;
 
-export const StyledCuisine = styled.p``;
-
 export const StyledRating = styled.p`
   width: 4rem;
+  
   text-align: center;
+  
   border-radius: 0.5rem;
   background-color: var(--rosehip-color);
 `;
 
 export const StyledPicture = styled.picture`
-  position: relative;
-  display: block;
   width: 100%;
   height: 20vh;
+  
+  position: relative;
+  display: block;
+
 `;
 
 
