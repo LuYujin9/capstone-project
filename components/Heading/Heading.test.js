e13e52a035c675d6bc40375043fb55dea6a54e47
Yujin Lu <123362292+LuYujin9@users.noreply.github.com>
2023-03-28 10:31:51 +0200

    Update Heading.test.js
    changed name of the test

--- components/Heading/Heading.test.js
+++ components/Heading/Heading.test.js
@@ -2,7 +2,7 @@ import Heading from "./Heading";
 import { render, screen } from "@testing-library/react";
 
 //test for back button has not finished.
-test("test Heading content", () => {
+test("display the content", () => {
   render(<Heading>title</Heading>);
   const element = screen.getByText(/title/i);
   expect(element).toBeInTheDocument();
