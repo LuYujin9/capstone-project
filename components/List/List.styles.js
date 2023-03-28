292eff1cd122c4bd2eacebaeea09e19075707494
Yujin Lu <123362292+LuYujin9@users.noreply.github.com>
2023-03-28 10:38:05 +0200

    Update List.styles.js
    removed the empty row set

--- components/List/List.styles.js
+++ components/List/List.styles.js
@@ -8,13 +8,10 @@ export const StyledImage = styled(Image)`
 export const StyledList = styled.ul`
   margin: 5rem 1vw;
   padding: 0;
-
   display: grid;
-
   @media only screen and (max-width: 799px) {
     gird: 80vw;
   }
-
   @media only screen and (min-width: 800px) {
     grid-template-columns: 1fr 1fr;
   }
@@ -22,22 +19,19 @@ export const StyledList = styled.ul`
     grid-template-columns: 1fr 1fr 1fr;
   }
 `;
+
 export const StyledLi = styled.li`
   margin: 0.5rem 0.5rem;
   padding: 5%;
   border-radius: 1rem;
-
   display: block;
-
   background-color: var(--white-color);
   box-shadow: 2px 2px 10px 3px var(--linen-color);
-
   width: 95%;
 `;
 
 export const StyledName = styled.p`
   width: 100%;
-
   font-size: 1.2rem;
 `;
 
@@ -53,12 +47,8 @@ export const StyledRating = styled.p`
 export const StyledPicture = styled.picture`
   position: relative;
   display: block;
-
   width: 100%;
   height: 20vh;
 `;
 
-/* 
-display: flex;
-  flex-direction: column;
-  align-items: center; */
+
