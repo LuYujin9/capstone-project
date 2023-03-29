cbe0dcbd7007599a86a553868b7cb40ca522c996
YujinLu9 <wuwujane@hotmail.com>
2023-03-29 19:52:47 +0200

    updata the test of Album, but there are still problems

--- components/Album/Album.test.js
+++ components/Album/Album.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Album from "./Album";
+import userEvent from "@testing-library/user-event";
 
 const testPhotos = [
   "/images/Carolaschlösschen1.jpg",
@@ -13,12 +14,49 @@ test("Image has alt text", () => {
   expect(photo).toHaveAttribute("alt", "a photo of the restaurant");
 });
 
-test("renders toNextPhotoButton", () => {
+test("doesn't render toLastPhotoButton when it shows the first photo", () => {
+  render(<Album photos={testPhotos} />);
+  const toLastPhotoButton = screen.queryByText(/to the last photo/i);
+  expect(toLastPhotoButton).not.toBeInTheDocument();
+});
+
+//Problem 1 : how to test "doesn't render toNextPhotoButton when it shows the last photo"
+test("doesn't render toNextPhotoButton when it shows the last photo", async () => {
+  const user = userEvent.setup();
+  render(<Album photos={testPhotos} />);
+  const toNextPhotoButton = screen.getByRole("button", {
+    name: /to the next photo/i,
+  });
+
+  await user.click(toNextPhotoButton);
+  await user.click(toNextPhotoButton);
+  await user.click(toNextPhotoButton);
+
+  const ToNextPhotoButtonWithLastPhoto = screen.queryByRole("button", {
+    name: /to the next photo/i,
+  });
+  expect(ToNextPhotoButtonWithLastPhoto).not.toBeInTheDocument();
+});
+
+//Problem 2: how to test "be called time", when the handle function is not a props.
+test("renders toNextPhotoButton and toLastPhotoButton", async () => {
+  const user = userEvent.setup();
   render(<Album photos={testPhotos} />);
 
   const toNextPhotoButton = screen.getByRole("button", {
     name: /to the next photo/i,
   });
 
+  await user.click(toNextPhotoButton);
+  await user.click(toNextPhotoButton);
+
+  const toLastPhotoButton = screen.getByRole("button", {
+    name: /to the last photo/i,
+  });
+  await user.click(toNextPhotoButton);
+
   expect(toNextPhotoButton).toBeInTheDocument();
+  expect(toLastPhotoButton).toBeInTheDocument();
+  expect(toNextPhotoButton).toBeCalledTimes(2);
+  expect(toLastPhotoButton).toBeCalledTimes(1);
 });
