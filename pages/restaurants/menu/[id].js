c72f059d8c9c9f27c602f1f6cf3eb5f182af3335
YujinLu9 <wuwujane@hotmail.com>
2023-03-31 08:47:44 +0200

    add the buttons to last page and next page, but the useState can't work

--- pages/restaurants/menu/[id].js
+++ pages/restaurants/menu/[id].js
@@ -4,17 +4,32 @@ import styled from "styled-components";
 import Heading from "../../../components/Heading/Heading";
 import FoodCard from "../../../components/FoodCard/FoodCard.js";
 import Image from "next/image.js";
+import { useState, useEffect } from "react";
 
 export default function Menu() {
-  const router = useRouter();
+  const [index, setIndex] = useState(0);
+  const [FoodsInCurrentPage, setFoodsInCurrentPage] = useState([]);
 
+  const router = useRouter();
   if (!router.isReady) {
     return <h2>Loading...</h2>;
   }
-
   const { id } = router.query;
+
   const restaurant = restaurants.find((restaurant) => restaurant.id === id);
   const foods = restaurant.foods;
+
+  //Problem is here. 5 foods show in one page, it can't work because it re-render too many times.
+  setFoodsInCurrentPage(foods.slice(index, index + 5));
+
+  function handleToNextPage() {
+    index < foods.length - 5 ? setIndex(index + 5) : null;
+  }
+  function handleToLastPage() {
+    index > 4 ? setIndex(index - 5) : null;
+  }
+  console.log(foods.slice(index, index + 5));
+  console.log(index);
 
   return (
     <>
@@ -27,14 +42,25 @@ export default function Menu() {
           alt="photo in background"
           fill
           sizes="(min-width: 768px) 100vw"
+          priority
         />
+        {index > 4 && (
+          <button aria-label="to next page" onClick={handleToLastPage}>
+            Last Page
+          </button>
+        )}
         <StyledList role="list">
-          {foods.map((food) => (
+          {FoodsInCurrentPage.map((food) => (
             <StyledListItem key={food.id}>
               <FoodCard food={food} />
             </StyledListItem>
           ))}
         </StyledList>
+        {index < foods.length - 5 && (
+          <button aria-label="to next page" onClick={handleToNextPage}>
+            Next Page
+          </button>
+        )}
       </StyledSection>
     </>
   );
