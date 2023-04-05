a90cd0707a1815892f9b77c40438f9081f3b6522
YujinLu9 <wuwujane@hotmail.com>
2023-04-05 12:38:16 +0200

    added the my-data-page and the link to favorites-page

---
+++ pages/my-data/index.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Heading from "../../components/Heading/Heading";
+
+export default function MyData() {
+  return (
+    <>
+      <Heading>Meine Daten</Heading>
+      <StyledSection>
+        <StyledLink
+          aria-label="link to favorites page"
+          href="/my-data/favorites"
+        >
+          Favoriten
+        </StyledLink>
+      </StyledSection>
+    </>
+  );
+}
+
+const StyledLink = styled(Link)`
+  padding: 0.5rem 3rem;
+
+  border-radius: 15px;
+  border: none;
+  color: var(--white-color);
+  background-color: var(--red-vine-color);
+`;
+
+const StyledSection = styled.section`
+  margin: 5rem auto;
+  padding: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media only screen and (max-width: 799px) {
+    width: 100vw;
+  }
+  @media only screen and (min-width: 800px) {
+    width: 640px;
+  }
+`;
