3f1d73df860e37acb3446260875e46620dfeed65
YujinLu9 <wuwujane@hotmail.com>
2023-04-05 10:52:10 +0200

    added some code, so that user can't schoose the date before now, when he reserves

--- components/RemainingSeatsFilter/RemainingSeatsFilter.js
+++ components/RemainingSeatsFilter/RemainingSeatsFilter.js
@@ -8,11 +8,17 @@ export default function RemainingSeatsFilter({ getRemainingSeats }) {
     getRemainingSeats(dataForSearch);
   }
 
+  const timeNow = new Date();
+  const year = timeNow.getFullYear();
+  const month = (timeNow.getMonth() + 1).toString().padStart(2, "0");
+  const date = timeNow.getDate().toString().padStart(2, "0");
+  const dateNow = `${year}-${month}-${date}`;
+
   return (
     <StyledForm aria-labelledby="formHeader" onSubmit={handleSubmit}>
       <StyledLegend id="formHeader">Suchen verfügbare Plätze</StyledLegend>
       <label htmlFor="date">Tag:</label>
-      <input type="date" name="date" id="date" required></input>
+      <input type="date" name="date" id="date" min={dateNow} required></input>
       <label htmlFor="time">Zeit:</label>
       <select name="time" id="time" required>
         <option value="11:00">11:00</option>
