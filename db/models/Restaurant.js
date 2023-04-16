a8daf72dfaf509d0506aff3428a13e398d36e038
YujinLu9 <wuwujane@hotmail.com>
2023-04-16 12:32:49 +0200

    updated the data of restaurants

--- db/models/Restaurant.js
+++ db/models/Restaurant.js
@@ -4,16 +4,18 @@ const { Schema } = mongoose;
 const restaurantSchema = new Schema({
   name: { type: String, required: true },
   rating: { type: Number, required: true },
-  telephoneNumber: String,
+  cuisine: { type: String, required: true },
   stadt: { type: String, required: true },
   address: { type: String, required: true },
+  lat: Number,
+  lng: Number,
+  telephoneNumber: String,
   maxSeats: { type: Number, required: true },
-  reserveInfos: [{ date: String, time: String, remainingSeats: Number }],
-  foods: [{ id: String, name: String, price: String, description: String }],
   potos: [String],
-  cuisine: { type: String, required: true },
   description: String,
+  foods: [{ id: String, name: String, price: String, description: String }],
   comments: [{ id: String, context: String, time: String }],
+  reserveInfos: [{ date: String, time: String, remainingSeats: Number }],
 });
 
 const Restaurant =
